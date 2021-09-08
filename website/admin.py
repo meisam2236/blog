4d3aba8beaefc58613874c80ab846cf0229c2098
Meisam Taghizadeh <meisam2236@gmail.com>
2021-09-08 21:55:50 +0430

    add custom admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(CustomUser)
-admin.site.register(Article)
-admin.site.register(Category)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content', 'category']
+    list_display = ['title', 'category', 'createdDate']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
