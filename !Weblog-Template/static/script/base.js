046c9dab253928eaa44983b05d1c420d32e55172
Mahdi A <81549809+Mahdi-Alizadeh96@users.noreply.github.com>
2021-09-12 02:45:26 +0430

    main page responsive

--- !Weblog-Template/static/script/base.js
+++ !Weblog-Template/static/script/base.js
@@ -7,9 +7,14 @@ $(document).ready(function() {
 // Menu 
 const   drowpMenu = document.querySelector('.main-header__menu li:nth-child(2)'),
         mainBaner = document.querySelector('.main-page-con'),
+        backdiv = document.querySelector('.menu-back'),
         drowpMenuBox = document.querySelector('.main-header__menu li:nth-child(2) div');
 drowpMenu.addEventListener('mouseover', ()=> {
-    drowpMenuBox.setAttribute('active','')
+    drowpMenuBox.setAttribute('active','');
+})
+
+backdiv.addEventListener('mouseleave', ()=> {
+    drowpMenuBox.removeAttribute('active');
 })
 
 drowpMenu.addEventListener('click', ()=> {
