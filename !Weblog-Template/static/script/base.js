46c03ba465836ce5a4bb06c21ecf72f0e7840281
Mahdi A <81549809+Mahdi-Alizadeh96@users.noreply.github.com>
2021-09-11 22:42:03 +0430

    main page fixed

--- !Weblog-Template/static/script/base.js
+++ !Weblog-Template/static/script/base.js
@@ -1,7 +1,16 @@
+// Tooltip
+$(document).ready(function() {
+    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+});
+// Tooltip
+
+// Menu 
 const   drowpMenu = document.querySelector('.main-header__menu li:nth-child(2)'),
         mainBaner = document.querySelector('.main-page-con'),
         drowpMenuBox = document.querySelector('.main-header__menu li:nth-child(2) div');
-
+drowpMenu.addEventListener('mouseover', ()=> {
+    drowpMenuBox.setAttribute('active','')
+})
 
 drowpMenu.addEventListener('click', ()=> {
     if(drowpMenuBox.hasAttribute('active')) {
