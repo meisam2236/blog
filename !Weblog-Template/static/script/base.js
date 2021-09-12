72a36eada84696fb6d7e29cb4921730c96d956af
Mahdi A <81549809+Mahdi-Alizadeh96@users.noreply.github.com>
2021-09-12 15:43:58 +0430

    main page design finished

--- !Weblog-Template/static/script/base.js
+++ !Weblog-Template/static/script/base.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 });
 // Tooltip
 
-// Menu 
+// Menu list
 const   drowpMenu = document.querySelector('.main-header__menu li:nth-child(2)'),
         mainBaner = document.querySelector('.main-page-con'),
         backdiv = document.querySelector('.menu-back'),
@@ -29,3 +29,32 @@ mainBaner.addEventListener('click', ()=> {
         drowpMenuBox.removeAttribute('active')
     }
 })
+
+// Menu respo
+
+const menuBox = document.querySelector('.main-header__menu ul');
+const colexit = document.querySelector('.collpase-menu i');
+const backBlack = document.querySelector('.blackBack');
+
+colexit.addEventListener('click', ()=> {
+
+  if(colexit.classList.contains('fa-bars')) {
+    colexit.classList = 'fas fa-times';
+    menuBox.style.right = 0;
+    backBlack.style.display = 'block';
+    console.log(backBlack)
+  }else {
+    colexit.classList = 'fas fa-bars';
+    menuBox.style.right = '-180px';
+    backBlack.style.display = 'none';
+    drowpMenuBox.removeAttribute('active');
+  }
+})
+
+backBlack.addEventListener('click', () => {
+  colexit.classList = 'fas fa-bars';
+  menuBox.style.right = '-180px';
+  backBlack.style.display = 'none';
+  drowpMenuBox.removeAttribute('active');
+})
+
